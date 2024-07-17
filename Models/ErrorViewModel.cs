052385de415c3c9304885896fe908f97ed1b53ae
57tannerpl <hugo-chan@outlook.com>
2024-07-17 12:50:32 +1000

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VMS_NET6._0_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/VideoFileInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace VMS_NET6._0_MVC.Models
+{
+    public class VideoFileInfo
+    {
+        public string FileName { get; set; }
+        public long FileSize { get; set; }
+    }
+}
